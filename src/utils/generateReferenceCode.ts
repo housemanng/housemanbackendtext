20ff0c55beca08ea2aa80b89b1de9cefbd8da097
CLEEQ24 <mr.chilakannamdi@gmail.com>
2025-08-22 20:20:32 +0100

    secondbackend

--- src/utils/generateReferenceCode.ts
+++ src/utils/generateReferenceCode.ts
@@ -1,10 +1,3 @@
-// utils/generateReferenceCode.ts
-// export const generateReferenceCode = (): string => {
-//           const min = 10000000000000000000n; // 20-digit minimum (10^19)
-//           const max = 99999999999999999999n; // 20-digit maximum (10^20 - 1)
-//           const randomNumber = BigInt(Math.floor(Number(min) + Math.random() * Number(max - min)));
-//           return randomNumber.toString();
-//         };
 
 export const generateReferenceCode = (): string => {
           let referenceCode = "";
